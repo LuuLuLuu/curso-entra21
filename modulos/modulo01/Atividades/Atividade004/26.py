a9768a63701ac1070cea9c29bd2ac281a3cfd8d4
LuuLuLuu <luisgustavoopp@gmail.com>
2023-10-18 10:09:21 -0300

    começando atividades incompletas

---
+++ modulos/modulo01/Atividades/Atividade004/26.py
@@ -0,0 +1,11 @@
+"""
+26. Crie um programa que auxilie no controle de gastos pessoais. Peça ao usuário para inserir suas despesas mensais nas seguintes categorias:
+Alimentação
+Moradia
+Transporte
+Lazer
+Saúde
+TV / Internet / Telefone
+Também peça ao usuário o seu salário mensal. O programa deve calcular o total de despesas, mostrar o percentual de cada categoria em relação ao total e informar se as suas despesas excedem o salário.
+"""
+

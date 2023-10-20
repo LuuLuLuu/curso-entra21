c7530965c783a6cdf5a3191d10967797752c70bb
LuuLuLuu <luisgustavoopp@gmail.com>
2023-10-20 16:06:13 -0300

    guardando

---
+++ modulos/modulo05/aula01/scripts/loops.js
@@ -0,0 +1,21 @@
+// 1) Last loop value
+    // 1
+
+
+// 2) Which values does the while loop show?
+	// 1. 1, 2, 3, 4
+	// 2. 1, 2, 3, 4, 5
+
+
+// 3) Which values get shown by the "for" loop?
+	// 1. 0,1,2,3,4
+	// 2. 0,1,2,3,4
+
+// 4) Output even numbers in the loop
+	// for (let +i = 2; i < 11; +i + 2) alert(i);
+
+	// for (let i = 2; i < 11; i++) {
+	// 	if (i % 2 === 0){
+	// 		alert(i);
+	// 	}
+	// }

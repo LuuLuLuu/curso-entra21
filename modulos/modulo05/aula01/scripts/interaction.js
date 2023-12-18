5786dc1c0d0e5b3507539a429817ee8587dfa7dd
Luis Gustavo <luisgustavoo@gmail.com>
2023-12-18 19:17:00 -0300

    voltando

--- modulos/modulo05/aula01/scripts/interaction.js
+++
@@ -1,3 +0,0 @@
-// 1) A simple page
-    // const userName = prompt("Type your name");
-    // alert(userName);
--- modulos/modulo05/aula01/scripts/types.js
+++
@@ -1,6 +0,0 @@
-// 1) String quotes
-    // The output
-    // hello 1
-    // hello "name"
-    // hello Ilya
-

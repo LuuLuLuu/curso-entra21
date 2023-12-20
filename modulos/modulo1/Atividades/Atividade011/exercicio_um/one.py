ab61691b4b8ae526471ab7faa7566e19cc47a044
Luis Gustavo <luisgustavoo@gmail.com>
2023-12-20 19:15:56 -0300

    terminando o exercicio do

---
+++ modulos/modulo1/Atividades/Atividade011/exercicio_um/one.py
@@ -0,0 +1,7 @@
+# 1. Escreva uma função que receba dois números como parâmetros e retorne a soma deles. 
+# Em seguida, escreva um teste usando pytest para verificar se a função está retornando 
+# o resultado correto.
+
+def func(a:int, b:int) -> int:
+    return a + b;
+
---
+++ modulos/modulo1/Atividades/Atividade011/exercicio_um/test_one.py
@@ -0,0 +1,5 @@
+from one import func
+
+def test_answer():
+    assert func(2, 3) == 5;
+
